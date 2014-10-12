7f1219a72c6b4d78e883d8cf8f65221223277182
Alicia Wilkerson <matalina@gmail.com>
2014-10-12 17:25:56 -0500

    trying to theme the demo

---
+++ config.rb
@@ -0,0 +1,33 @@
+# Require any additional compass plugins here.
+
+# Tell compass where to find local extensions
+# If you followed directions and ran 'gem install modular-scale' comment the next two lines out:
+# extensions_dir = "assets/sass/extensions"
+
+# Compass::Frameworks.register('modular-scale', :path => File.expand_path("#{extensions_dir}/modular-scale"))
+
+# Uncomment these to use regular Ruby gems.
+# require 'modular-scale'
+# require 'sassy-math'
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "public/assets/css"
+sass_dir = "assets/sass"
+images_dir = "public/assets/images"
+
+# You can select your preferred output style here (can be overridden via the command line) :nested or :expanded or :compact or :compressed:
+output_style = :compact
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
